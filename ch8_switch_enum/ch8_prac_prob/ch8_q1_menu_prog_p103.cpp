8f606393fe3e45cd47cc333023ff1426507d02ce
Ming Xin <ace.xin.ivy@gmail.com>
2024-04-30 19:54:27 -0700

    working on ch8_q1.

---
+++ ch8_switch_enum/ch8_prac_prob/ch8_q1_menu_prog_p103.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+enum class foodType {
+    hotdog = 1,
+    sandwich = 2,
+    deli = 3,
+};
+
+void menu() {
+    foodType selectedFood;
+    int food;
+    do {
+        /* code */
+    } while (/* condition */);
+}
+
+int main() {
+    return 0;
+}
